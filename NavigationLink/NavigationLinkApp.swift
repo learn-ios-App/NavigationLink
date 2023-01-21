b3a20d3505a7a385fbf0ba02c977daff94aaca6a
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-01-21 14:30:49 +0900

    Initial Commit

---
+++ NavigationLink/NavigationLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLinkApp.swift
+//  NavigationLink
+//
+//  Created by 渡邊魁優 on 2023/01/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
